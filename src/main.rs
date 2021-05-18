3205b9eb9f4fcaa5a4933ff787f8f221fd64f6d4
Ido Kanner <idok@linesip.com>
2021-05-18 21:08:28 +0300

    starting to use struct and vectors

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use std::ffi::CStr;
 use std::fs;
 use std::os::linux::fs::MetadataExt;
 
+#[derive(Debug)]
+struct Info {
+    pub pid: u64,
+    pub user_name: String,
+    pub group_name: String,
+}
+
 fn get_user_name(uid: u32) -> String {
     let name = unsafe {
         let passwd = libc::getpwuid(uid);
@@ -22,20 +29,27 @@ fn get_group_name(gid: u32) -> String {
     group_name
 }
 
-fn print_process_info(pid: u64, file_name: String) -> Result<(), String> {
+fn process_info(pid: u64, file_name: String) -> Result<Info, String> {
     let meta =
         fs::metadata(file_name).map_err(|_| String::from("Unable to read /proc directory"))?;
 
     let user_name = get_user_name(meta.st_uid());
     let group_name = get_group_name(meta.st_gid());
-    println!("{}", format!("{:5} {:5} {:10}", user_name, group_name, pid));
 
-    Ok(())
+    let info = Info {
+        pid,
+        user_name,
+        group_name,
+    };
+
+    Ok(info)
 }
 
-fn iter_proc() -> Result<(), String> {
+fn iter_proc() -> Result<Vec<Info>, String> {
     let paths =
         fs::read_dir("/proc").map_err(|_| String::from("Unable to read /proc directory"))?;
+
+    let mut info_list: Vec<Info> = Vec::new();
     for path in paths {
         let path_info = path.unwrap();
         let full_path = path_info.path();
@@ -54,11 +68,14 @@ fn iter_proc() -> Result<(), String> {
         }
 
         let full_path_string = String::from(full_path.to_str().unwrap());
-        print_process_info(pid.unwrap(), full_path_string).map_err(|e| e)?;
+        let info = process_info(pid.unwrap(), full_path_string).map_err(|e| e)?;
+        info_list.push(info);
     }
-    Ok(())
+    Ok(info_list)
 }
 
 fn main() -> Result<(), String> {
-    iter_proc()
+    let info_list = iter_proc().map_err(|e| e)?;
+    println!("{:#?}", info_list);
+    Ok(())
 }
